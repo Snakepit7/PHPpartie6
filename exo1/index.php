6c84745116e70c0bbb109018c59aa419645784f1
Snakepit7 <julienfollet71@gmail.com>
2018-11-20 14:53:51 +0100

    exos

---
+++ exo1/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exo1</title>
+  </head>
+  <body>
+      <?php
+       echo (isset($_GET['firstname']) ? htmlentities($_GET['firstname']) : "Le prénom est introuvable"); 
+       echo (isset($_GET['lastname']) ? htmlentities($_GET['lastname']) : "Le prénom est introuvable");
+      ?>
+  </body>
+</html>
